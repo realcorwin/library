d5178b119ab7519687998e15e17ae9193911d849
DiK <realcorwin@mail.ru>
2019-01-24 23:34:25 +0100

    add beforeDeleteEvent

---
+++ src/main/java/dik/library/event/CascadeDeleteMongoEventListener.java
@@ -0,0 +1,27 @@
+package dik.library.event;
+
+import dik.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeDeleteEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class CascadeDeleteMongoEventListener extends AbstractMongoEventListener<Object> {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public void onBeforeDelete(BeforeDeleteEvent<Object> event) throws RuntimeException {
+        if (Objects.equals(event.getCollectionName(), "genre") || Objects.equals(event.getCollectionName(), "author")) {
+            String id = null;
+            for (Object key : event.getSource().values()) {
+                id = String.valueOf(key);
+            }
+            if (bookRepository.findByGenreId(id) != null || bookRepository.findByAuthorId(id) != null) throw new RuntimeException();
+        }
+    }
+}
--- src/main/java/dik/library/repository/BookRepository.java
+++ src/main/java/dik/library/repository/BookRepository.java
@@ -4,4 +4,8 @@ import dik.library.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface BookRepository extends MongoRepository<Book, String>, BookRepositoryCustom {
+
+    public Book findByAuthorId(String authorId);
+
+    public Book findByGenreId(String genreId);
 }
