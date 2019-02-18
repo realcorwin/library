09ff333270b259cfe58033089a577a1fcc96096f
DiK <realcorwin@mail.ru>
2019-02-18 13:08:11 +0100

    rest controllers, tests and swagger ui

--- src/main/java/dik/library/rest/AuthorRestController.java
+++ src/main/java/dik/library/rest/AuthorRestController.java
@@ -2,6 +2,8 @@ package dik.library.rest;
 
 import dik.library.model.Author;
 import dik.library.service.AuthorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,33 +16,39 @@ import java.util.List;
 
 @RestController
 //@RequestMapping(AuthorRestController.REST_URL)
+@Api(value="REST API для работы с авторами", description = "REST API для работы с авторами")
 public class AuthorRestController {
     //static final String REST_URL = "/rest/author";
 
     @Autowired
     AuthorService authorService;
 
+    @ApiOperation(value = "Показать всех авторов")
     @GetMapping(value = "/rest/author", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Author> getAll() {
         return authorService.getAllAuthor();
     }
 
+    @ApiOperation(value = "Показать автора")
     @GetMapping(value = "/rest/author/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Author get(@PathVariable("id") String id) {
         return authorService.getById(id);
     }
 
+    @ApiOperation(value = "Добавить автора")
     @PostMapping(value = "/rest/author", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody Author author) {
         authorService.insert(author.getFirstName(), author.getSecondName());
     }
 
+    @ApiOperation(value = "Удалить автора")
     @DeleteMapping(value = "/rest/author/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") String id) {
         authorService.deleteById(id);
     }
 
+    @ApiOperation(value = "Изменить автора")
     @PutMapping(value = "/rest/author/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody Author author, @PathVariable("id") String id) {
         author.setId(id);
--- src/main/java/dik/library/rest/BookRestController.java
+++ src/main/java/dik/library/rest/BookRestController.java
@@ -2,6 +2,8 @@ package dik.library.rest;
 
 import dik.library.model.Book;
 import dik.library.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,33 +16,40 @@ import java.util.List;
 
 @RestController
 //@RequestMapping(BookRestController.REST_URL)
+@Api(value="REST API для работы с книгами", description = "REST API для работы с книгами")
 public class BookRestController {
     //static final String REST_URL = "/rest/book";
 
     @Autowired
     BookService bookService;
 
+    @ApiOperation(value = "Показать все книги")
     @GetMapping(value = "/rest/book", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Book> getAll() {
         return bookService.getAllBook();
     }
 
+    @ApiOperation(value = "Показать книгу")
     @GetMapping(value = "/rest/book/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book get(@PathVariable("id") String id) {
         return bookService.getById(id);
     }
 
+    @ApiOperation(value = "Добавить книгу")
     @PostMapping(value = "/rest/book", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void add(@RequestBody Book book) {
+        book.setId(null);
         bookService.insert(book);
     }
 
+    @ApiOperation(value = "Удалить книгу")
     @DeleteMapping(value = "/rest/book/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") String id) {
         bookService.deleteById(id);
     }
 
+    @ApiOperation(value = "Изменить книгу")
     @PutMapping(value = "/rest/book/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody Book book, @PathVariable("id") String id) {
         book.setId(id);
