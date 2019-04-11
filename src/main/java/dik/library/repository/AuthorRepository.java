022bbdb332a000cbddb9c813844a570b52ca8c0a
DiK <realcorwin@mail.ru>
2019-04-11 23:33:43 +0200

    add data rest

--- src/main/java/dik/library/repository/AuthorRepository.java
+++ src/main/java/dik/library/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package dik.library.repository;
 
 import dik.library.model.Author;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "author")
 public interface AuthorRepository extends MongoRepository<Author, String> {
 }
